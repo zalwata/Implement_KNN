91ebca72c00b470be32bf0022b9797e92ac0748d
zalwata <zalwata@hotmail.com>
2022-04-16 15:43:11 -0700

    found out there is starter code.

--- Implement_KNN/main.py
+++ Implement_KNN/main.py
@@ -9,16 +9,36 @@ import numpy as np
 from numpy.linalg import norm
 
 #function
-def distanceAlgorithmEuclidean(x, y):
-    distance = np.linalg.norm(x-y)
-    return distance
+
 
 #class
 class KNN:
 
+    def __init__(self, k, random_state=None):
+        self.k = k
+        self.random_state = random_state
+        self.labels_ = None
+
+    # YOUR ADDITIONAL CODE HERE
+
+    def fit(self, features: np.ndarray, labels: np.ndarray) -> None:
+        '''
+        Create the KNN model (or not)
+        '''
+        # YOUR CODE HERE
+
+    def predict(self, features: np.ndarray) -> np.array:
+        '''
+        Predict the labels for the input features given the
+        training instances.
+        '''
+    # YOUR CODE HERE
+
+
+
+
 
 #application driver
-df = np.array([[1,2,3],[2,1,3],[3,2,1]])
 
 
 
